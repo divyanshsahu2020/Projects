5cebafdcddd4acf3c834dbb830824fe7404b92af
Divyansh Sahu <80671629+divyanshsahu2020@users.noreply.github.com>
2023-09-15 13:54:52 +0530

    Delete plantdisease directory

--- plantdisease/README.md
+++
@@ -1,2 +0,0 @@
-Plant Disease Detection Flask working model
-DataSet Link : https://www.kaggle.com/datasets/emmarex/plantdisease
--- plantdisease/asss.py
+++
@@ -1,7 +0,0 @@
-import queue
-stack=queue.LifoQueue()
-s=input()
-for i in s:
-    stack.put(i)
-while stack:
-    print(stack.get(),end="")
